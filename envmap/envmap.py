Fixed mistake in inner product computation

envmap/envmap.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class EnvMap():
 
     def __init__(self, num_gaussians):
-        self._weights = nn.Parameter(torch.zeros((num_gaussians, 3, 5), device="cuda")  + 0.01, requires_grad=True)
+        self._weights = nn.Parameter(torch.zeros((num_gaussians, 3, 5), device="cuda") + 0.01, requires_grad=True)
 
     @property
     def get_gaussians(self):

render.py
@@ -9,6 +9,7 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
+import matplotlib.cm
 import torch
 from scene import Scene
 import os
@@ -58,8 +59,22 @@ def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParam
 
         point_in_cam = torch.matmul(homogemous_points_world, w2c)
 
-        
-        
+        depth = torch.sqrt(torch.sum(point_in_cam**2,1))
+        # depth = (depth - 0) / (100 - 0)
+
+        depth = depth.unsqueeze(1).repeat(1,3)
+
+        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+        rendering = render(scene.getTrainCameras()[0], gaussians, pipeline, background, override_color=depth)["render"].permute(1,2,0)
+
+
+        import numpy as np
+        import matplotlib
+        import cv2
+        depth_cpu = rendering.detach().cpu().numpy()
+        cv2.imshow("ol", depth_cpu)
+        cv2.waitKey()
         exit()
         
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]

scene/gaussian_model.py
@@ -175,7 +175,7 @@ class GaussianModel:
             {'params': [self._opacity], 'lr': training_args.opacity_lr, "name": "opacity", "per_gaussian": True},
             {'params': [self._scaling], 'lr': training_args.scaling_lr, "name": "scaling", "per_gaussian": True},
             {'params': [self._rotation], 'lr': training_args.rotation_lr, "name": "rotation", "per_gaussian": True},
-            {'params': [self._envmap.get_gaussians], 'lr': training_args.feature_lr, "name": "envmap", "per_gaussian": False}
+            # {'params': [self._envmap.get_gaussians], 'lr': training_args.feature_lr, "name": "envmap", "per_gaussian": False}
         ]
 
         self.optimizer = torch.optim.Adam(l, lr=0.0, eps=1e-15)

utils/sg_utils.py
@@ -33,7 +33,7 @@ def calculate_colors(gaussian_data, normal_data, envmap_data):
     l_m = g_l + em_l
     alphas = g_a * em_a
 
-    fraction = torch.exp(d_m - l_m) - torch.exp(-d_m-l_m) / (d_m + 1e-10)
+    fraction = (torch.exp(d_m - l_m) - torch.exp(-d_m-l_m)) / (d_m + 1e-10)
 
 
     return torch.sum(2.0 * torch.pi * alphas * fraction, dim=1)